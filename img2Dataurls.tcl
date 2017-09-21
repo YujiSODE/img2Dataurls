885819424207ae0f7065db5d4d874d6ef88f1374
Yuji Sode <yuji.sode@gmail.com>
2017-09-21 21:54:35 +0900

    Update img2Dataurls.tcl

--- img2Dataurls.tcl
+++ img2Dataurls.tcl
@@ -6,7 +6,7 @@
 #	This software is released under the MIT License.
 #	See LICENSE or http://opensource.org/licenses/mit-license.php
 ##===================================================================
-#It outputs file that has an array of dataurls, corresponding to image file with the given extension in the current directory, and returns the output filename.
+#It outputs file that has an array of dataurls, corresponding to image files with the given extension in the current directory, and returns the output filename.
 #=== Synopsis ===
 #::img2Dataurls::getUrls exName1;
 #or
@@ -21,7 +21,7 @@ namespace eval ::img2Dataurls {
 	#=== dataurl64/dataurl64.tcl (Yuji SODE,2017): the MIT License; https://gist.github.com/YujiSODE/009cdfb080e273ec506a1ef24acd050c ===
 	#it returns a data url with base64-encoded data.
 	proc dataurl64 {fileName {type text/plain}} {set c [open $fileName r];fconfigure $c -encoding binary -translation binary;set v [read $c];close $c;return "data:$type;base64,[binary encode base64 $v]";};
-	#it outputs file that has an array of dataurls, corresponding to image file with the given extension in the current directory, and returns the output filename
+	#it outputs file that has an array of dataurls, corresponding to image files with the given extension in the current directory, and returns the output filename
 	proc getUrls {exName} {
 		# - $exName: name of image filename extension
 		set T [clock seconds];
